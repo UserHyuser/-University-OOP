4f0e793fd725ae90a4ec3462d894f866f4d63e2d
UserHyuser <drachevskii@yandex.ru>
2019-03-16 13:51:01 +0700

    added field

--- bird_atd.h
+++ bird_atd.h
@@ -8,6 +8,7 @@ namespace simple_animals {
 	class bird : public animal {
 		char name[20];
 		int fly; // перелетная ли птица 0 - нет 1 - да
+		int age = 0; //возраст
 	public:
 		// переопределяем интерфейс класса
 		void InData(ifstream &ifst);  // ввод
